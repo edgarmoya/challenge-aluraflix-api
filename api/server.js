efd87dc531e5a9fdb27ee801a9721c133b282b42
edgarmoya <edgarcmoya@gmail.com>
2024-07-14 13:49:53 -0400

    Permitir escritura

--- api/server.js
+++ api/server.js
@@ -1,7 +1,16 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require("json-server");
+
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+
+// Uncomment to allow write operations
+const fs = require("fs");
+const path = require("path");
+const filePath = path.join("db.json");
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db);
+
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
